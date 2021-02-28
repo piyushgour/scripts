07e5b1c1106eb760597965f53b37244bf7fde83a
piyushgour <39912076+piyushgour@users.noreply.github.com>
2021-02-28 10:23:20 +0530

    Delete old branches

---
+++ delete_old_branch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ECHO='echo '
+for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'master$'); do
+  if [[ "$(git log $branch --since "6 months ago" | wc -l)" -eq 0 ]]; then
+    if [[ "$DRY_RUN" = "false" ]]; then
+      echo " Kuch bhi"
+    fi
+    local_branch_name=$(echo "$branch" | sed 's/remotes\/origin\///')
+    echo "$local_branch_name"
+    $ECHO git branch -d $local_branch_name
+    $ECHO git push origin --delete $local_branch_name
+  fi
+done
